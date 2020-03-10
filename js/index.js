7461862b08bbe343a06c987aba3ab23d71595c42
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-10 10:04:52 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ var height = "74"
 
 if (name === "susan"){
   console.log("the name is correct")
+} else {
+  console.log("Expected the name to be Susan")
 }
 // Don't worry about this
 module.exports = { name, height
